fd01d59a6c2afa6eb6530fc085e45a7885e018c7
jackiesafari <jcqln_jackie@yahoo.com>
2025-03-14 19:56:59 -0700

    Testing faucet and smart contracts

--- examples/hello/hardhat.config.ts
+++ examples/hello/hardhat.config.ts
@@ -5,12 +5,23 @@ import * as dotenv from "dotenv";
 import "./tasks/deploy";
 import "@zetachain/localnet/tasks";
 import "@zetachain/toolkit/tasks";
+// Remove this line: import "@zetachain/hardhat-plugin";
 import { getHardhatConfig } from "@zetachain/toolkit/client";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
   ...getHardhatConfig({ accounts: [process.env.PRIVATE_KEY] }),
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.26",
+      },
+      {
+        version: "0.8.28",
+      }
+    ]
+  },
 };
 
 export default config;
